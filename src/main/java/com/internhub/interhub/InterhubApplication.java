15f13958f31550147c24fe477f7128f613d7eda1
Said NAJIM <saidnajim.ma@gmail.com>
2023-12-06 20:24:28 +0100

    first commit

---
+++ src/main/java/com/internhub/interhub/InterhubApplication.java
@@ -0,0 +1,13 @@
+package com.internhub.interhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/internhub/interhub/InterhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internhub.interhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
