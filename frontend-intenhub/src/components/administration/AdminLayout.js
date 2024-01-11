a48ba03e59ee4e0d08ee5d2c6c29b818f15a46c3
Said NAJIM <saidnajim.ma@gmail.com>
2024-01-11 23:15:25 +0100

    interface modalite 1

--- frontend-intenhub/src/components/administration/AdminLayout.js
+++ frontend-intenhub/src/components/administration/AdminLayout.js
@@ -30,12 +30,12 @@ export default function AdminLayout({ children }) {
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="./form-elements.html">
+                  <Link to="/admin/modalitesstages" className="nav-link" href="./form-elements.html">
                     <span className="nav-link-icon d-md-none d-lg-inline-block">
                       <svg xmlns="http://www.w3.org/2000/svg" className="icon" width={24} height={24} viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round" > <path stroke="none" d="M0 0h24v24H0z" fill="none" /> <path d="M9 11l3 3l8 -8" /> <path d="M20 12v6a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h9" /> </svg>
                     </span>
                     <span className="nav-link-title">Modalités stages</span>
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
                   <a className="nav-link" href="./form-elements.html">
