bb1844c6acc033a9b968228ba2f0ee034e7bdbd7
laze03 <Lazrak.edu@gmail.com>
2024-01-12 13:41:17 +0100

    Front:mesStages for student

--- frontend-intenhub/src/components/etudiant/TableStudent.js
+++ frontend-intenhub/src/components/etudiant/TableStudent.js
@@ -1,29 +1,28 @@
-export default function TableStudent({children}) {
+export default function TableStudent({ children }) {
   return (
     <div className="page-body">
-    <div className="container-xl">
-      <div className="card">
-        <div className="card-body">
-          <div id="table-default" className="table-responsive">
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Date debut</th>
-                <th>Date fin</th>
-                <th>Entreprise</th>
-                <th>Niveau</th>
-                <th>Numéro</th>
-                <th>Statut</th>
-                <th>Infos</th>
-                <th>Supprimer</th>
-              </tr>
-            </thead>
-              {children}
-          </table>
+      <div className="container-xl">
+        <div className="card">
+          <div className="card-body">
+            <div id="table-default" className="table-responsive">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Date debut</th>
+                    <th>Date fin</th>
+                    <th>Entreprise</th>
+                    <th>Niveau</th>
+                    <th>Numéro</th>
+                    <th>Statut</th>
+                    <th>Infos</th>
+                  </tr>
+                </thead>
+                {children}
+              </table>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    </div>
-  )
+  );
 }
