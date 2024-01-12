366d513512a63ba54946db211b68dd8af36776dd
laze03 <Lazrak.edu@gmail.com>
2024-01-12 13:41:17 +0100

    Front:mesStages for student

--- frontend-intenhub/src/components/etudiant/RowsStudent.js
+++ frontend-intenhub/src/components/etudiant/RowsStudent.js
@@ -1,140 +1,281 @@
-import React from 'react'
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function RowsStudent() {
-  const internships = [
-    {
-      "idStage": 123,
-      "dateDebut": "2023-01-02",
-      "dateFin": "2024-01-02",
-      "entreprise" : "Intenhub",
-      "villeEntreprise" : "Rabat",
-      "contactEntreprise" : "06xxxxxxxx",
-      "enacdrantEntreprise" : "Ilyas ABDELLAOUI",
-      "contactEncadrant" : "ilyasabdellaoui@pro.ma", 
-      "profEncadrant" : "Karim SBAINA",
-      "niveau" : "2A",
-      "numero" : "1er",
-      "statut" : "En révision"
-    },
-    {
-      "idStage": 1234,
-      "dateDebut": "2025-01-02",
-      "dateFin": "2026-01-02",
-      "entreprise" : "Good",
-      "villeEntreprise" : "Rabat",
-      "contactEntreprise" : "06xxxxxxxx",
-      "enacdrantEntreprise" : "Ilyas ABDELLAOUI",
-      "contactEncadrant" : "ilyasabdellaoui@pro.ma", 
-      "profEncadrant" : "Karim SBAINA",
-      "niveau" : "1A",
-      "numero" : "2ème",
-      "statut" : "En cours"   
-    }
-  ];
+  const [internships, setInternships] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      return await axios
+        .post("http://localhost:8080/student/mesStages", {
+          numEtu: localStorage.getItem("numEtu"),
+        })
+        .then((response) => {
+          console.log(response);
+          const uniqueIntenrships = Array.from(
+            new Set(
+              response.data.map((internship) => ({
+                dateDebut: new Date(internship.dateDebut).toLocaleDateString(
+                  "fr-FR",
+                  {
+                    year: "numeric",
+                    month: "2-digit",
+                    day: "2-digit",
+                  }
+                ),
+                dateFin: new Date(internship.dateFin).toLocaleDateString(
+                  "fr-FR",
+                  {
+                    year: "numeric",
+                    month: "2-digit",
+                    day: "2-digit",
+                  }
+                ),
+                idStage: internship.numStage,
+                entreprise: internship.tuteur.entreprise.raisonSocial,
+                villeEntreprise: internship.tuteur.entreprise.villeEntr,
+                contactEntreprise: internship.tuteur.entreprise.contact,
+                enacdrantEntreprise:
+                  internship.tuteur.nomTut + " " + internship.tuteur.prenomTut,
+                contactEncadrant: "ilyasabdellaoui@pro.ma",
+                profEncadrant:
+                  internship.professeur.nomProf +
+                  " " +
+                  internship.professeur.prenomProf,
+                niveau: internship.annee,
+                numero: internship.typeStage.codeType,
+                statut: internship.status,
+              }))
+            )
+          );
+          setInternships(uniqueIntenrships);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    fetchData();
+  }, []);
+  // const internships = [
+  //   {
+  //     idStage: 123,
+  //     dateDebut: "2023-01-02",
+  //     dateFin: "2024-01-02",
+  //     entreprise: "Intenhub",
+  //     villeEntreprise: "Rabat",
+  //     contactEntreprise: "06xxxxxxxx",
+  //     enacdrantEntreprise: "Ilyas ABDELLAOUI",
+  //     contactEncadrant: "ilyasabdellaoui@pro.ma",
+  //     profEncadrant: "Karim SBAINA",
+  //     niveau: "2A",
+  //     numero: "1er",
+  //     statut: "En révision",
+  //   },
+  //   {
+  //     idStage: 1234,
+  //     dateDebut: "2025-01-02",
+  //     dateFin: "2026-01-02",
+  //     entreprise: "Good",
+  //     villeEntreprise: "Rabat",
+  //     contactEntreprise: "06xxxxxxxx",
+  //     enacdrantEntreprise: "Ilyas ABDELLAOUI",
+  //     contactEncadrant: "ilyasabdellaoui@pro.ma",
+  //     profEncadrant: "Karim SBAINA",
+  //     niveau: "1A",
+  //     numero: "2ème",
+  //     statut: "En cours",
+  //   },
+  // ];
   return (
     <tbody className="table-tbody">
       {internships.map((internship) => (
         <tr className="align-middle" key={internship.idStage}>
-        {/* Lharba: this is UNIX timestamp = sec elapsed since Jan 1, 1970 (the Unix epoch) */}
-        <td className="sort-date-debut" data-date={Math.floor(new Date(internship.dateDebut).getTime() / 1000)}>
-          {internship.dateDebut}
-        </td>
-        <td className="sort-date-fin" data-date={Math.floor(new Date(internship.dateFin).getTime() / 1000)}>
-          {internship.dateFin}
-        </td>
-        <td>{internship.entreprise}</td>
-        <td>{internship.niveau}</td>
-        <td>{internship.numero}</td>
-        <td>{internship.statut}</td>
-        <td><button className="btn btn-warning w-100" data-bs-toggle="modal" data-bs-target={`#modal-${internship.idStage}`}>
-            Détails
-        </button></td>
-        {/* TO DO : Delete the intetrnship record (by internship.idStage) */}
-        { internship.statut === "En révision" && 
-        <td className="ms-auto">
-          <a href="/" className="btn btn-danger" data-bs-dismiss="modal">Supp.</a>
-        </td> }
-        <div className="modal modal-blur fade" id={`modal-${internship.idStage}`} tabIndex={-1} aria-hidden="true" style={{display: 'none'}}>
-          <div className="modal-dialog modal-lg modal-dialog-centered" role="document">
-            <form className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">Détails stage</h5>
-                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
-              </div>
-              <div className="modal-body">
-                <div className="row">
-                  <div className="col-lg-4">
-                    <div className="mb-3">
-                      <label className="form-label">Date début</label>
-                      <input type="text" value={internship.dateDebut} className="form-control" disabled />
+          {/* Lharba: this is UNIX timestamp = sec elapsed since Jan 1, 1970 (the Unix epoch) */}
+          <td
+            className="sort-date-debut"
+            data-date={Math.floor(
+              new Date(internship.dateDebut).getTime() / 1000
+            )}
+          >
+            {internship.dateDebut}
+          </td>
+          <td
+            className="sort-date-fin"
+            data-date={Math.floor(
+              new Date(internship.dateFin).getTime() / 1000
+            )}
+          >
+            {internship.dateFin}
+          </td>
+          <td>{internship.entreprise}</td>
+          <td>{internship.niveau}</td>
+          <td>{internship.numero}</td>
+          <td>{internship.statut}</td>
+          <td>
+            <button
+              className="btn btn-warning w-100"
+              data-bs-toggle="modal"
+              data-bs-target={`#modal-${internship.idStage}`}
+            >
+              Détails
+            </button>
+          </td>
+
+          <div
+            className="modal modal-blur fade"
+            id={`modal-${internship.idStage}`}
+            tabIndex={-1}
+            aria-hidden="true"
+            style={{ display: "none" }}
+          >
+            <div
+              className="modal-dialog modal-lg modal-dialog-centered"
+              role="document"
+            >
+              <form className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title">Détails stage</h5>
+                  <button
+                    type="button"
+                    className="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  />
+                </div>
+                <div className="modal-body">
+                  <div className="row">
+                    <div className="col-lg-4">
+                      <div className="mb-3">
+                        <label className="form-label">Date début</label>
+                        <input
+                          type="text"
+                          value={internship.dateDebut}
+                          className="form-control"
+                          disabled
+                        />
+                      </div>
                     </div>
-                  </div>
-                  <div className="col-lg-4">
-                    <div className="mb-3">
-                      <label className="form-label">Date fin</label>
-                      <input type="text" value={internship.dateFin} className="form-control" disabled />
+                    <div className="col-lg-4">
+                      <div className="mb-3">
+                        <label className="form-label">Date fin</label>
+                        <input
+                          type="text"
+                          value={internship.dateFin}
+                          className="form-control"
+                          disabled
+                        />
+                      </div>
                     </div>
-                  </div>
-                  <div className="col-lg-2">
-                    <div className="mb-3">
-                      <label className="form-label">Année</label>
-                      <input type="text" value={internship.niveau} className="form-control" disabled/>
+                    <div className="col-lg-2">
+                      <div className="mb-3">
+                        <label className="form-label">Année</label>
+                        <input
+                          type="text"
+                          value={internship.niveau}
+                          className="form-control"
+                          disabled
+                        />
+                      </div>
                     </div>
-                  </div>
-                  <div className="col-lg-2">
-                    <div className="mb-3">
-                      <label className="form-label">Ordre</label>
-                      <input type="text" value={internship.numero} className="form-control" disabled />
+                    <div className="col-lg-2">
+                      <div className="mb-3">
+                        <label className="form-label">Ordre</label>
+                        <input
+                          type="text"
+                          value={internship.numero}
+                          className="form-control"
+                          disabled
+                        />
+                      </div>
                     </div>
-                  </div>
-                  <div className="col-lg-4">
-                    <div className="mb-3">
-                      <label className="form-label">Nom entreprise</label>
-                      <input type="text" value={internship.entreprise} className="form-control" disabled />
+                    <div className="col-lg-4">
+                      <div className="mb-3">
+                        <label className="form-label">Nom entreprise</label>
+                        <input
+                          type="text"
+                          value={internship.entreprise}
+                          className="form-control"
+                          disabled
+                        />
+                      </div>
                     </div>
-                  </div>
-                  <div className="col-lg-4">
-                    <div className="mb-3">
-                      <label className="form-label">Ville entreprise</label>
-                      <input type="text" value={internship.villeEntreprise} className="form-control" disabled />
+                    <div className="col-lg-4">
+                      <div className="mb-3">
+                        <label className="form-label">Ville entreprise</label>
+                        <input
+                          type="text"
+                          value={internship.villeEntreprise}
+                          className="form-control"
+                          disabled
+                        />
+                      </div>
                     </div>
-                  </div>
-                  <div className="col-lg-4">
-                    <div className="mb-3">
-                      <label className="form-label">Contact entreprise</label>
-                      <input type="text" value={internship.contactEntreprise} placeholder="Email ou GSM" className="form-control" disabled />
+                    <div className="col-lg-4">
+                      <div className="mb-3">
+                        <label className="form-label">Contact entreprise</label>
+                        <input
+                          type="text"
+                          value={internship.contactEntreprise}
+                          placeholder="Email ou GSM"
+                          className="form-control"
+                          disabled
+                        />
+                      </div>
                     </div>
-                  </div>
-                  <div className="col-lg-6">
-                    <div className="mb-3">
-                      <label className="form-label">Encadrant entreprise</label>
-                      <input type="text" value={internship.enacdrantEntreprise} placeholder="Nom complet" className="form-control" disabled />
+                    <div className="col-lg-6">
+                      <div className="mb-3">
+                        <label className="form-label">
+                          Encadrant entreprise
+                        </label>
+                        <input
+                          type="text"
+                          value={internship.enacdrantEntreprise}
+                          placeholder="Nom complet"
+                          className="form-control"
+                          disabled
+                        />
+                      </div>
                     </div>
-                  </div>
-                  <div className="col-lg-6">
-                    <div className="mb-3">
-                      <label className="form-label">Contact encadrant</label>
-                      <input type="text" value={internship.contactEncadrant} placeholder="Email ou GSM" className="form-control" disabled />
+                    <div className="col-lg-6">
+                      <div className="mb-3">
+                        <label className="form-label">Contact encadrant</label>
+                        <input
+                          type="text"
+                          value={internship.contactEncadrant}
+                          placeholder="Email ou GSM"
+                          className="form-control"
+                          disabled
+                        />
+                      </div>
                     </div>
-                  </div>
-                  <div className="col-lg-6">
-                    <div className="mb-3">
-                      <label className="form-label">Professeur encadrant</label>
-                      <input type="text" value={internship.profEncadrant} disabled className="form-control" />
+                    <div className="col-lg-6">
+                      <div className="mb-3">
+                        <label className="form-label">
+                          Professeur encadrant
+                        </label>
+                        <input
+                          type="text"
+                          value={internship.profEncadrant}
+                          disabled
+                          className="form-control"
+                        />
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <div className="modal-footer">
-                <a href="/" className="btn btn-link link-secondary" data-bs-dismiss="modal">
-                  Annuler
-                </a>
-              </div>
-            </form>
+                <div className="modal-footer">
+                  <a
+                    href="/"
+                    className="btn btn-link link-secondary"
+                    data-bs-dismiss="modal"
+                  >
+                    Annuler
+                  </a>
+                </div>
+              </form>
+            </div>
           </div>
-        </div>
         </tr>
       ))}
     </tbody>
-  )
+  );
 }
