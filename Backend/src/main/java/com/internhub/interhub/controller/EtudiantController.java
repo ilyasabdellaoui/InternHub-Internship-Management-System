680e531286793720699a0ad49480a234eb94576e
ilyasabdellaoui <ilyas.abdellaoui21@gmail.com>
2024-01-12 12:44:01 +0100

    Feat(Controller): meStages Endpoint + created User credentials

--- Backend/src/main/java/com/internhub/interhub/controller/EtudiantController.java
+++ Backend/src/main/java/com/internhub/interhub/controller/EtudiantController.java
@@ -1,19 +1,19 @@
 package com.internhub.interhub.controller;
 
-import com.internhub.interhub.model.Etudiant;
-import com.internhub.interhub.model.Promo;
-import com.internhub.interhub.model.Professeur;
-import com.internhub.interhub.repository.EtudiantRepository;
-import com.internhub.interhub.repository.PromoRepository;
-import com.internhub.interhub.repository.ProfesseurRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.internhub.interhub.model.*;
+import com.internhub.interhub.repository.*;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.Year;
+import java.util.ArrayList;
 import java.util.List;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/student")
@@ -22,16 +22,19 @@ public class EtudiantController {
 
     private final EtudiantRepository etudiantRepository;
     private final PromoRepository promoRepository;
-    private final ProfesseurRepository professeurRepository;
+    private final StageRepository stageRepository;
+    private final UserRepository userRepository;
 
-    @Autowired
     public EtudiantController(
             EtudiantRepository etudiantRepository,
             PromoRepository promoRepository,
-            ProfesseurRepository professeurRepository) {
+            ProfesseurRepository professeurRepository,
+            StageRepository stageRepository,
+            UserRepository userRepository) {
         this.etudiantRepository = etudiantRepository;
         this.promoRepository = promoRepository;
-        this.professeurRepository = professeurRepository;
+        this.stageRepository = stageRepository;
+        this.userRepository = userRepository;
     }
 
     @PostMapping("/get")
@@ -69,12 +72,60 @@ public class EtudiantController {
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Promo with the given year not found");
             }
             etudiantRepository.save(etudiant);
+            User user = new User();
+            Role role = new Role();
+            role.setRoleID(3);
+
+            user.setEtudiant(etudiant);
+            String email = etudiant.getNomEtu() + etudiant.getNumEtu() + "@ecm.ma";
+            user.setRole(role);
+            user.setUserEmail(email);
+            String generatedPassword = generatePassword(String.valueOf(etudiant.getNumEtu()));
+            user.setUserPassword(generatedPassword);
+            userRepository.save(user);
             return ResponseEntity.status(HttpStatus.CREATED).body(etudiant.getNumEtu());
         } catch (DataIntegrityViolationException e) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Student with the given ID already exists.");
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error adding student");
+        }
+    }
+
+    @PostMapping("mesStages")
+    public ResponseEntity<?> mesStages(@RequestBody Map<String, String> requestBody) {
+        try {
+            int numEtu = Integer.parseInt(requestBody.get("numEtu"));
+            Etudiant student = etudiantRepository.findByNumEtu(numEtu);
+            List<Stage> stages = new ArrayList<>();
+            if (student != null) {
+                Stage stage = stageRepository.findStageByEtudiant(student);
+                if (stage != null) {
+                    stages.add(stage);
+                }
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student with the given ID have no recoreded Stage not found.");
+            }
+            return ResponseEntity.ok(stages);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error retrieving stages for Student");
+        }
+    }
+
+    public static String generatePassword(String uniqueId) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = messageDigest.digest(uniqueId.getBytes());
+
+            String password = Base64.getEncoder().encodeToString(hashBytes);
+
+            int maxLength = 10;
+            return password.substring(0, Math.min(password.length(), maxLength));
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
